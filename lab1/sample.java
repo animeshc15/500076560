2063714c3a851d285a0ccf099b84a28862a3e4d5
animeshc@15 <animeshc15@gmail.com>
2021-03-05 13:40:49 +0530

    First Commit

---
+++ lab1/sample.java
@@ -0,0 +1,12 @@
+class Sample
+{
+public static void main(String[] args)
+{
+int a=10,b=20,sum;
+sum=a+b;
+// Sum of 10 and 20 is 30
+//printf("The sum of %d and %d is %d,a,b,sum);
+System.out.println("sum of 10 and 20 is:"+sum+" ");
+}
+
+}
